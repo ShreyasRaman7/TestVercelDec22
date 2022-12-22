d9907b0c1358f1d18cad038c30cb0706bac4dd01
ShreyasRaman7 <71574041+ShreyasRaman7@users.noreply.github.com>
2022-12-22 16:49:53 -0500

    added components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
-import HomePage from './HomePage';
-import Tester from "./Tester";
-import { HashRouter, Route, Routes,Link, BrowserRouter } from 'react-router-dom';
-import NavBar from './NavBar';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  Outlet
+} from "react-router-dom";
 import ResponsiveAppBar from './ResponsiveAppBar';
+import SoccerTransfers from './SoccerTransfers';
+import CommentsSection from './CommentsSection';
+import SoccerHighlights from './SoccerHighlights';
+import xg from "./LiveSoccerGameStates";
+import LiveSoccerGameStates from './LiveSoccerGameStates';
 import NFLSchedule from './NFLSchedule';
+import ToDoList from './ToDoList';
+import WeatherComponent from './WeatherComponent';
+import ChatApp from './ChatApp';
+import TicTacToe from './TicTacToe';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core';
+import HomePage from './HomePage';
+import background1 from "./background1.jpg";
+import WeightedGradeCalculator from './WeightedGradeCalculator';
+import PolynomialGraph from './PolynomialGraph';
+import { HashRouter } from 'react-router-dom';
+
+
 
 function App() {
-
   return (
-     <div className="App"  >
-      <header className="App-header" >
-        <ResponsiveAppBar/>
-      <NavBar/>
-      <BrowserRouter>
-        <Routes>
-        <Route path="/" element={<HomePage />}/>
+    
+    <div className="App"  >
+      <ResponsiveAppBar/>
+      <header className="App-header">
+      
+      <BrowserRouter >
+      <Routes>
+         
+        <Route  path="/SoccerHighlights" element={<SoccerHighlights />} />
+        <Route path="/comments" element={<CommentsSection />} />
+        <Route path="/xg" element={<LiveSoccerGameStates />} />
         <Route  path="/nfl" element={<NFLSchedule />} />
-        <Route path="tester" element={<Tester />} />
-        </Routes>
-      
-</BrowserRouter>
-      
+        <Route path="/soccerTransfers" element={<SoccerTransfers />} />
+        <Route path="/ToDoList" element={<ToDoList />} />
+        <Route path="/Weather" element={<WeatherComponent />} />
+        <Route path="/ChatApp" element={<ChatApp />} />
+        <Route path="/Tictactoe" element={<TicTacToe />} />
+        <Route path="/gradeCalculator" element={<WeightedGradeCalculator />} />
+        <Route path="/graph" element={<PolynomialGraph />} />
+        <Route path="/" element={<HomePage />} />
+
+      </Routes>
+    </BrowserRouter>
       </header>
     </div>
   );
