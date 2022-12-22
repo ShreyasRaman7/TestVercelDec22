0fd3fd6644f3a466692e394f87eb41fdb99c4ec1
ShreyasRaman7 <71574041+ShreyasRaman7@users.noreply.github.com>
2022-12-22 12:03:41 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './HomePage';
+import Tester from "./Tester";
+import { HashRouter, Route, Routes,Link, BrowserRouter } from 'react-router-dom';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     <div className="App"  >
+      <header className="App-header" >
+      {/* <Link to="/">home</Link>
+      <Link to="/tester">tester</Link> */}
+      {/* <HashRouter>
+      <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/tester" element={<Tester />} />
+        </Routes>  
+      </HashRouter> */}
+      <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<HomePage />}/>
+
+        <Route path="tester" element={<Tester />} />
+        </Routes>
+      
+</BrowserRouter>
+      
       </header>
     </div>
   );
---
+++ src/Tester.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Tester = (props) => {
+  return (
+    <div>
+      <h1>Hello, Tester!</h1>
+    </div>
+  );
+};
+
+export default Tester;
