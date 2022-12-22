0a96da6cce86f24803b2a17e21ec84f696de1c44
ShreyasRaman7 <71574041+ShreyasRaman7@users.noreply.github.com>
2022-12-22 12:10:40 -0500

    second push

--- src/App.js
+++ src/App.js
@@ -3,24 +3,17 @@ import './App.css';
 import HomePage from './HomePage';
 import Tester from "./Tester";
 import { HashRouter, Route, Routes,Link, BrowserRouter } from 'react-router-dom';
+import NavBar from './NavBar';
 
 function App() {
 
   return (
      <div className="App"  >
       <header className="App-header" >
-      {/* <Link to="/">home</Link>
-      <Link to="/tester">tester</Link> */}
-      {/* <HashRouter>
-      <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/tester" element={<Tester />} />
-        </Routes>  
-      </HashRouter> */}
+      <NavBar/>
       <BrowserRouter>
         <Routes>
         <Route path="/" element={<HomePage />}/>
-
         <Route path="tester" element={<Tester />} />
         </Routes>
       
---
+++ src/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div>NavBar
+        <a href='/'>HomePage: </a>
+        <a href='/tester'>TesterPage: </a>
+
+
+
+    </div>
+    
+  )
+}
