4fe318b71a0d9b328d01b7b06b1a25aef93610c2
ShreyasRaman7 <71574041+ShreyasRaman7@users.noreply.github.com>
2022-12-23 21:58:34 -0500

    color changes

--- src/NFLSchedule.js
+++ src/NFLSchedule.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Box from '@mui/material/Box';
+let key1 =0
 function NFLSchedule() {
   const [schedule, setSchedule] = useState([]);
   const [darkMode, setDarkMode] = useState(false);
-
+  const blueGradient = 'radial-gradient(circle, rgba(129,191,98,1) 0%, rgba(185,255,191,1) 65%)';
+  const redOrangeGradient = 'radial-gradient(circle, rgba(255,72,72,1) 0%, rgba(255,101,101,1) 65%)';
+  const yellowGradient = 'radial-gradient(circle, rgba(243,247,170,1) 0%, rgba(255,207,101,1) 65%)';
 
   function toggleDarkMode() {
     setDarkMode(!darkMode);
@@ -29,9 +34,9 @@ function NFLSchedule() {
   }, []);
 
   return (
-    <div  className={darkMode ? 'dark-mode' : 'light-mode'}><button onClick={toggleDarkMode}>Toggle Dark Mode</button>
-    
-    <table >
+    <div  className={darkMode ? 'dark-mode' : 'light-mode'}>
+    🏈 NFL Stats for 2022:
+    {/* <table >
       <thead>
         <tr>
         </tr>
@@ -39,6 +44,7 @@ function NFLSchedule() {
       <tbody>
         {console.log(schedule._embedded)}
         {schedule._embedded && schedule._embedded.teamWinStatsList.map(game => (
+          
           <tr  >
             <td>{game.name}</td>
             <td>Wins: <span className='winsLoss'>{game.wins}</span></td>
@@ -47,7 +53,43 @@ function NFLSchedule() {
           </tr>
         ))}
       </tbody>
-    </table>
+    </table> */}
+
+    <Box sx={{ width: '100%',  bgcolor: 'background.paper' }}>
+    {schedule._embedded && schedule._embedded.teamWinStatsList.map(game => (
+         <List style={{padding: '10px', color:'#0047AB', borderRadius: '5px'  }}>
+         {game && console.log(game) }
+         <Box sx={{
+        background:
+          game && game.winRatePercentage < 0.5
+            ? redOrangeGradient
+            : game && game.winRatePercentage === 0.5
+            ? yellowGradient
+            : blueGradient
+      }}>
+         <ListItem style={{
+                 border: '1px solid #ccc',
+                 borderRadius: '5px',
+                 marginBottom: '10px',
+                 padding: '10px',
+               }} key={key1 += 1}>  {game.name} ❖ <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}> <h5 style={{marginRight:'10px 0', color:'#f8f4f4' ,  fontSize:'1'}} > {''}</h5> <h4 style={{color:'black'}}>         Wins: {game.wins}  ,Losses: {game.losses}  ,Win Rate : {game.winRatePercentage}%   </h4> </div>
+               </ListItem>
+               </Box>
+           {/* {game.map((myEvent) => (
+             <ListItem style={{
+                 border: '1px solid #ccc',
+                 borderRadius: '5px',
+                 marginBottom: '10px',
+                 padding: '10px',
+                 backgroundColor: '#f4f4f4',
+               }} key={key1 += 1}> Year: {myEvent.name}: {myEvent.name}</ListItem>
+             
+             
+           ))} */}
+         </List>
+        ))}
+    
+    </Box>
     </div>
   );
 }
